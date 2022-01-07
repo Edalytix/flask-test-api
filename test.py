42b084f579c33a68dcb1a25e419876d38cf17746
Aditya0918 <75171134+Aditya0918@users.noreply.github.com>
2022-01-07 07:38:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -107,9 +107,7 @@ class fetch_from_s3(Resource):
 
                     try:
 
-                    
-                        file_path="C:\\Users\\Admin\\Desktop\\Survideo"
-                        file_path=file_path+"\\"+s3_obj
+                        file_path=s3_obj
                         s3_bucket.download_file(obj_path,file_path)
 
                         if object_list[-1]=='wav':
@@ -129,7 +127,7 @@ class fetch_from_s3(Resource):
                         if object_list[-1]=='mp3':
                             
                             print("Converting....")
-                            output_file="C:\\Users\\Admin\\Desktop\\Survideo"+"\\"+object_list[0]+".wav"
+                            output_file=object_list[0]+".wav"
                             #print(file_path)
                             #print(output_file)
                             sound = AudioSegment.from_mp3(file_path)
